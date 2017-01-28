f90c38dc80568f5c22b79da722b075ef1c82c7af
Kim Lindqvist <kealin@users.noreply.github.com>
2017-01-28 20:38:18 +0200

    Update index.js
    Removed line that was using cookieparser

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/feedback', feedback);
